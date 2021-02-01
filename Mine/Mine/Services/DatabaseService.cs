a50a2759f474a97d2f7f537b7c5368dc4ef3ddc2
Michael Arvan <arvanmichael@seattleu.edu>
2021-01-31 17:33:24 -0800

    Added IndexAsync in DataBaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -54,10 +54,15 @@ namespace Mine.Services
         {
             throw new NotImplementedException();
         }
-
-        public Task<IEnumerable<ItemModel>> IndexAsync(bool forceRefresh = false)
+        /// <summary>
+        /// Index Implemenation of Items
+        /// </summary>
+        /// <param name="forceRefresh">Bool value</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<ItemModel>> IndexAsync(bool forceRefresh = false)
         {
-            throw new NotImplementedException();
+            var result = await Database.Table<ItemModel>().ToListAsync();
+            return result;
         }
     }
 }
