785fcc9fd6f734de3365a6558cbff9d9c106e37d
Michael Arvan <arvanmichael@seattleu.edu>
2021-02-06 14:03:52 -0800

    Add RollDice_InValid_Roll_0_Dice_10_Should_Return_Zero unit test to DiceHelperTests.cs

--- UnitTest/Helpers/DiceHelperTests.cs
+++ UnitTest/Helpers/DiceHelperTests.cs
@@ -83,5 +83,20 @@ namespace UnitTest.Helpers
             // Assert 
             Assert.AreEqual(0, result);
         }
+
+        [Test]
+        public void RollDice_InValid_Roll_0_Dice_10_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 10);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
+
     }
 }
